12646b1187330a89468df9d25d773e883d594953
Aaron Battman <aaron.k.battman@outlook.com>
2021-08-30 17:58:24 +0100

    quick, route, method an views for farm index/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # DSL https://guides.rubyonrails.org/routing.html
+  resources :farms, only: [:index, :show]
 end
