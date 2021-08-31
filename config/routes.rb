129c79e9b1208709fb50539ac1a340162a47bf17
Aaron Battman <aaron.k.battman@outlook.com>
2021-08-31 12:42:35 +0100

    minimal farm changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :farms, only: [:index, :show]
+  resources :farms
   resources :products, only: [:index, :show, :create]
 end
