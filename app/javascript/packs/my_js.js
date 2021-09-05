2eb761bf1c80689cb0d48a5b9957451c40898253
Kateryna Ueno <katerynaueno@gmail.com>
2021-09-05 15:13:39 +0100

    deleted some text

--- app/javascript/packs/my_js.js
+++ app/javascript/packs/my_js.js
@@ -1,13 +1,3 @@
-// document.getElementById('form').addEventListener('click', function () {
-//   evt.preventDefault();
-//   alert("Added to basket");
-// });
-
-
-
-// addEventListener('click', function () {
-//   alert("Random Message");
-// });
 
 const form = document.querySelectorAll("form");
 form.forEach(function(element) {
