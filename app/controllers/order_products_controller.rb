dc1bc5953a491e60868e9a7d76f64e0c8e5fdbfa
Kateryna Ueno <katerynaueno@gmail.com>
2021-09-08 13:24:07 +0100

    Stripe is working

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -18,50 +18,6 @@ class OrderProductsController < ApplicationController
       end
     end
   end
-  # def create
-    # @order = Order.find_by(user: current_user)
-    # @product = Product.find(params[:product_id])
-  #   # if clicked => quanity += 1
-  #   @order_product = OrderProduct.create!(order: @order, product: @product, amount: product.price, state: 'pending', quantity: order_product_params[:quantity])
-  #   session = Stripe::Checkout::Session.create(
-  #   payment_method_types: ['card'],
-  #   order.order_products.each do |order_product|
-  #     line_items: [{
-  #       name: product.name,
-  #       images: [product.photo_url],
-  #       amount: product.price_cents,
-  #       currency: 'gbp',
-  #       quantity: 1
-  #     }],
-  #     end
-  #     success_url: order_products_url(order),
-  #     cancel_url: order_products_url(order)
-  #     )
-
-    # respond_to do |format|
-    #   if @order_product
-    #     format.html { redirect_to request.referrer }
-    #     format.json # Follow the classic Rails flow and look for a create.json view
-    #   else
-    #     format.html { render 'farms/show' }
-    #     format.json # Follow the classic Rails flow and look for a create.json view
-    #   end
-    # end
-
-  # def create
-  # @product = Product.find(params[:product_id])
-  # @order = Order.create!(product: product, amount: product.price, state: 'pending', user: current_user)
-
-
-  #   success_url: order_url(order),
-  #   cancel_url: order_url(order)
-  # )
-
-  # order.update(checkout_session_id: session.id)
-  # redirect_to new_order_payment_path(order)
-  # end
-  #     # redirect_to request.referrer
-  # end
 
   def increase_quantity
     @order = Order.find_by(user: current_user)
