5385fdab67527a7e14aee2be0db9bbadd74ebb5e
Olga Kolesova <olka.kolesova@gmail.com>
2021-09-11 21:55:21 +0100

    changed authentication

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,5 +1,5 @@
 class FarmsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show, :index]
+  skip_before_action :authenticate_user!, only: [:index]
   before_action :set_farm, only: [:show, :edit, :update, :destroy]
 
   def index
